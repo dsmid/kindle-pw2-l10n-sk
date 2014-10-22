bf3839241e06c2527920802ab4de8114e068b69e
Andrej Tobola <andrej.tobola+gifthub@gmail.com>
2014-10-22 16:51:11 +0200

    Update resources.js

--- translation_5.4.3.1/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
+++ translation_5.4.3.1/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
@@ -1,28 +1,28 @@
-Close	
-Details for this advert are no longer available. Turn on wireless to receive the latest content.	
-Details for this advert are no longer available.	
-Device Not Registered	
-Error	
-Offer Expired	
-Order Cancelled	
-Parental Controls are enabled on your Kindle. Purchases are not allowed.	
-Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	
-Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	
-Purchase Limit Reached	
-Purchased by Accident? Cancel this Order	
-Purchasing Locked	
-Special Offer	
-Thank you for your order	
-To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	
-View All Special Offers	
-View Special Offers	
-We are unable to process your request. Please try again later.	
-You already have {count} order(s) pending for this special offer.	
-You have other orders pending. Click here to view	
-You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	
-You must register your Kindle to make a purchase.	
-Your order will be placed when you connect to wireless.	
-Your purchase could not be completed. This offer has expired.	
-Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	
-Your purchase could not be completed. You have reached the maximum purchase quantity.	
-{customerName}, you have nearly finished.	
+Close	Zatvoriť
+Details for this advert are no longer available. Turn on wireless to receive the latest content.	Detaily tejto ponuky nie sú viac k dispozícii. Pre načítanie najnovšej ponuky zapnite bezdrôtovú sieť.
+Details for this advert are no longer available.	Detaily tejto ponuky nie sú viac k dispozícii.
+Device Not Registered	Zariadenie nie je registrované
+Error	Chyba
+Offer Expired	Ponuka skončila
+Order Cancelled	Objednávka zrušená
+Parental Controls are enabled on your Kindle. Purchases are not allowed.	Na vašom Kindli je zapnutá rodičovská kontrola.
+Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	Pre stiahnutie najnovších špeciálnych ponúk sa prosím pripojte na bezdrôtovú sieť. Keď budú nové ponuky dostupné, zobrazia sa automaticky.
+Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	Aby ste mohli prijímať aktuálne ponuky, zaregistrujte si váš Kindle. Keď budú nové ponuky dostupné, zobrazia sa automaticky.
+Purchase Limit Reached	Dosiahli ste nákupný limit.
+Purchased by Accident? Cancel this Order	Zakúpené omylom? Zrušte túto objednávku
+Purchasing Locked	Nákup uzamknutý
+Special Offer	Špeciálna ponuka
+Thank you for your order	Ďakujeme za vašu objednávku
+To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	Na dokončenie objednávky na Amazone sa pripojte na bezdrôtovú sieť počas nasledujúcich {purchaseDaysRemaining} dní. Na hlavnej obrazovke sa vám zobrazí kupón a bude vám tiež zaslané upozornenie na váš e-mailový účet. Kupón bude obsahovať inštrukcie na uplatnenie tejto ponuky.
+View All Special Offers	Ukázať všetky špeciálne ponuky
+View Special Offers	Ukázať špeciálne ponuky
+We are unable to process your request. Please try again later.	Nebolo možné spracovať vašu požiadavku. Skúste to prosím neskôr.
+You already have {count} order(s) pending for this special offer.	Pre túto špeciálnu ponuku už máte {count} nevybavenú(é) objednávku(y).
+You have other orders pending. Click here to view	Máte ďalšie nevybavené objednávky.
+You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	Úspešne ste zrušili objednávku titulu {dealTitle} od {merchantName}.
+You must register your Kindle to make a purchase.	Pre uskutočnenie nákupu musíte váš Kindle zaregistrovať.
+Your order will be placed when you connect to wireless.	Objednávka bude vybavená po pripojení sa na bezdrôtovú sieť.
+Your purchase could not be completed. This offer has expired.	Nákup nemohol byť dokončený. Táto ponuka skončila.
+Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	Nákup nemohol byť dokončený. Momentálne máme technické problémy. Skúste to prosím neskôr.
+Your purchase could not be completed. You have reached the maximum purchase quantity.	Nákup nemohol byť dokončený. Prekročili ste maximálny počet zakúpení.
+{customerName}, you have nearly finished.	{customerName}, ste takmer na konci.
