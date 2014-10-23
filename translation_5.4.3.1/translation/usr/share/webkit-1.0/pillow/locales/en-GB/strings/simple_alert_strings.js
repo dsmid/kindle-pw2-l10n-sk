15ed53793a4c84ec5849ecaedfbd38a198eb24d4
Andrej Tobola <andrej.tobola+gifthub@gmail.com>
2014-10-23 12:06:31 +0200

    Update simple_alert_strings.js

--- translation_5.4.3.1/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/simple_alert_strings.js
+++ translation_5.4.3.1/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/simple_alert_strings.js
@@ -1,95 +1,95 @@
-Active Content Locked	
-Aeroplane Mode Locked	
-Aeroplane Mode	
-An unexpected error has occurred. This can happen if you recently disconnected your Kindle from a computer without ejecting it safely. Please wait while your Kindle attempts to resolve this error.	
-An update is available for the following fonts: ({language}). Your Kindle must restart to complete the update.	
-Application Error	
-Battery Level is Low	
-Cancel	
-Close	
-Cloud Locked	
-Debugging Scripts	
-Details for this Special Offer are no longer available. Turn on wireless to receive the latest content.	
-Details for this Special Offer are no longer available.	
-Details for this Special Offer are not available while Parental Controls are enabled.	
-Device Error	
-Device Not Registered	
-Device Reset Pending	
-Device Restart Pending	
-Error Starting Content	
-Error	
-Extended debugging scripts are currently enabled	
-Framework Error	
-Goodreads on Kindle Locked	
-Java Heap Low	
-Kindle Special Offers	
-Kindle Store Locked	
-Low Memory	
-MP3 Player Locked	
-No MP3 Files Found	
-OK	
-Offer Expired	
-Parental Controls are enabled on your Kindle. Access to Cloud is locked.	
-Parental Controls are enabled on your Kindle. Access to Goodreads on Kindle is locked.	
-Parental Controls are enabled on your Kindle. Kindle Store is locked.	
-Parental Controls are enabled on your Kindle. Purchases are not allowed.	
-Parental Controls are enabled on your Kindle. Web Browser is locked.	
-Please select from the following options:	
-Power	
-Purchase Limit Reached	
-Purchases Pending	
-Purchasing Locked	
-Restart Required	
-Restart	
-Restarting your Kindle	
-Screen Off	
-Settings Menu Locked	
-Shipping Mode Failed	
-Social Networks Locked	
-The Java Heap has been low for too long.	
-The Wi-Fi network you are connecting to requires you to sign in before accessing the Internet. Do you wish to go to the Web Browser to sign in to: {essid}?	
-The Wi-Fi network you are connecting to requires you to sign in before accessing the Internet. Do you wish to go to the Web Browser to sign in?	
-The application is not responding. Please wait while your Kindle restarts.	
-The item failed to close properly. Unless you close this dialog box your device will restart in {restartTime,plural,=0 {# seconds} one {# second} other {# seconds}}.	
-The selected application could not be started. Please try again.	
-The selected content could not be opened. Please try again.	
-The title could not be opened.	
-There is not enough Kindle memory to add or edit notes or marks for this document. Please make more Kindle memory available by removing content from the Home screen.	
-This Kindle has been instructed to restart. This process will begin shortly. Please wait until the device has fully restarted.	
-This is a managed device. Access to Cloud is locked. For information, contact: {contactInfo}.	
-This is a managed device. Access to Goodreads on Kindle is locked. For information, contact: {contactInfo}.	
-This is a managed device. Active Content is locked. For information, contact: {contactInfo}.	
-This is a managed device. Aeroplane Mode is locked. For information, contact: {contactInfo}.	
-This is a managed device. It has been instructed to reset to factory defaults, which will remove all downloaded and transferred content. This process will begin shortly. Please wait until this process is complete. For information, contact: {contactInfo}.	
-This is a managed device. Kindle Store is locked. For information, contact: {contactInfo}.	
-This is a managed device. MP3 Player is locked. For information, contact: {contactInfo}.	
-This is a managed device. Settings Menu is locked. For information, contact: {contactInfo}.	
-This is a managed device. Social Network access is locked. For information, contact: {contactInfo}.	
-This is a managed device. Toggle Wireless is locked. For information, contact: {contactInfo}.	
-This is a managed device. Web Browser is locked. For information, contact: {contactInfo}.	
-This is a managed device. Wi-Fi Settings configuration is locked. For information, contact: {contactInfo}.	
-To play MP3 files, you must first copy them from your computer to the "music" folder on your Kindle.	
-Toggle Wireless Locked	
-Turn On Wireless?	
-Unable to Open Content	
-Unable to enter shipping mode owing to battery level falling below permissible range.	
-Unable to enter shipping mode owing to missing dictionaries or fonts.	
-Update Later	
-Update Now	
-Updating Fonts	
-Very Low Battery	
-We are unable to process your request. Please try again later.	
-Web Browser Locked	
-Wi-Fi Login Required	
-Wi-Fi Settings Locked	
-Wireless is turned off. Do you want to turn on wireless?	
-You already have {count,plural,=0 {no orders pending} one {an order pending} other { # orders pending}} for this special offer.	
-You must register your Kindle to make a purchase.	
-Your Kindle fonts are updating. This can take a moment...	
-Your Kindle is unable to establish a wireless connection. Do you want to turn off Aeroplane Mode?	
-Your battery is almost depleted and your Kindle will soon shut down. To continue using your Kindle, connect it to a power source.	
-Your battery is running low. Please charge your Kindle. If your wireless is turned on, try turning it off to extend your reading time.	
-Your battery is running low. Please charge your Kindle.	
-Your purchase could not be completed. This offer has expired.	
-Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	
-Your purchase could not be completed. You have reached the maximum purchase quantity.	
+Active Content Locked	Aktívny obsah uzamknutý
+Aeroplane Mode Locked	Letový režim uzamknutý
+Aeroplane Mode	Letový režim
+An unexpected error has occurred. This can happen if you recently disconnected your Kindle from a computer without ejecting it safely. Please wait while your Kindle attempts to resolve this error.	Nastala neočakávaná chyba. Môže sa stať v prípade, ak neodoberiete bezpečne Kindle zo systému a náhle ho odpojíte od počítača. Počkajte prosím, Kindle sa pokúsi odstrániť túto chybu.
+An update is available for the following fonts: ({language}). Your Kindle must restart to complete the update.	Je dostupná aktualizácia nasledujúcich písiem: ({language}). Pre dokončenie aktualizácie sa musí váš Kindle reštartovať.
+Application Error	Chyba aplikácie
+Battery Level is Low	Nízka úroveň batérie
+Cancel	Zrušiť
+Close	Zatvoriť
+Cloud Locked	Cloud server uzamknutý
+Debugging Scripts	Prebieha ladenie skriptov
+Details for this Special Offer are no longer available. Turn on wireless to receive the latest content.	Detaily tejto špeciálnej ponuky nie sú viac k dispozícii. Pre načítanie aktuálnych ponúk zapnite bezdrôtovu sieť.
+Details for this Special Offer are no longer available.	Detaily tejto špeciálnej ponuky nie sú viac k dispozícii
+Details for this Special Offer are not available while Parental Controls are enabled.	Detaily tejto špeciálnej ponuky nie sú k dispozícii, ak je zapnutá rodičovská kontrola.
+Device Error	Chyba zariadenia
+Device Not Registered	Zariadenie nie je registrované
+Device Reset Pending	Čaká sa na vyresetovanie zariadenia
+Device Restart Pending	Čaká sa na reštart zariadenia
+Error Starting Content	Chyba pri spúšťaní obsahu
+Error	Chyba
+Extended debugging scripts are currently enabled	Rozšírené ladenie skriptov je momentálne zapnuté
+Framework Error	Chyba Frameworku
+Goodreads on Kindle Locked	Kindle Goodreads uzamknutý
+Java Heap Low	Nedostatok pamäte Java Heap
+Kindle Special Offers	Špeciálne ponuky Kindle
+Kindle Store Locked	Obchod Kindle uzamknutý
+Low Memory	Nedostatok pamäte
+MP3 Player Locked	MP3 prehrávač uzamknutý
+No MP3 Files Found	Nenájdené žiadne MP3 súbory
+OK	OK
+Offer Expired	Ponuka skončila
+Parental Controls are enabled on your Kindle. Access to Cloud is locked.	Je zapnutá rodičovská kontrola. Prístup na Cloud server uzamknutý.
+Parental Controls are enabled on your Kindle. Access to Goodreads on Kindle is locked.	Je zapnutá rodičovská kontrola. Prístup na Goodreads uzamknutý.
+Parental Controls are enabled on your Kindle. Kindle Store is locked.	Je zapnutá rodičovská kontrola. Obchod Kindle uzamknutý.
+Parental Controls are enabled on your Kindle. Purchases are not allowed.	Je zapnutá rodičovská kontrola. Nakupovanie nie je povolené.
+Parental Controls are enabled on your Kindle. Web Browser is locked.	Je zapnutá rodičovská kontrola. Internetový prehliadač uzamknutý.
+Please select from the following options:	Vyberte si prosím z nasledujúcich možností:
+Power	Napájanie
+Purchase Limit Reached	Dosiahli ste limit zakúpení
+Purchases Pending	Nevybavené nákupy
+Purchasing Locked	Nakupovanie uzamknuté
+Restart Required	Vyžadovaný reštart
+Restart	Reštart
+Restarting your Kindle	Kindle sa reštartuje
+Screen Off	Vypnúť obrazovku
+Settings Menu Locked	Menu Nastavenia uzamknuté
+Shipping Mode Failed	Nepodarilo sa nastaviť prepravný mód
+Social Networks Locked	Sociálne siete uzamknuté
+The Java Heap has been low for too long.	Nedostatok pamäte Java Heap trvá príliš dlho.
+The Wi-Fi network you are connecting to requires you to sign in before accessing the Internet. Do you wish to go to the Web Browser to sign in to: {essid}?	Wi-Fi sieť, na ktorú sa prihlasujete vyžaduje prihlásenie. Chcete spustiť internetový prehliadač a prihlásiť sa do: {essid}?
+The Wi-Fi network you are connecting to requires you to sign in before accessing the Internet. Do you wish to go to the Web Browser to sign in?	Wi-Fi sieť, na ktorú sa prihlasujete vyžaduje prihlásenie. Chcete spustiť internetový prehliadač a prihlásiť sa?
+The application is not responding. Please wait while your Kindle restarts.	Aplikácia neodpovedá. Počkajte prosím na dokončenie reštartu.
+The item failed to close properly. Unless you close this dialog box your device will restart in {restartTime,plural,=0 {# seconds} one {# second} other {# seconds}}.	Položku sa nepodarilo správne zatvoriť. Pokiaľ neuzatvoríte tento dialóg, vaše zariadenie sa reštartuje za {restartTime,plural,=0 {# sekúnd} one {# sekundu} other {# sekúnd}}.
+The selected application could not be started. Please try again.	Zvolenú aplikáciu sa nepodarilo spustiť. Skúste to prosím znovu.
+The selected content could not be opened. Please try again.	Zvolený obsah sa nepodarilo otvoriť. Skúste to prosím znovu.
+The title could not be opened.	Titul sa nepodarilo otvoriť.
+There is not enough Kindle memory to add or edit notes or marks for this document. Please make more Kindle memory available by removing content from the Home screen.	Kindle má nedostatok pamäte pre pridanie alebo úpravu poznámok alebo značiek v tomto dokumente. Uvoľnite prosím viac pamäte odstránením obsahu z hlavnej obrazovky.
+This Kindle has been instructed to restart. This process will begin shortly. Please wait until the device has fully restarted.	Kindle musí byť reštarovaný. Tento proces sa čoskoro spustí. Počkajte prosím na úplné dokončenie reštartu vášho zariadenia.
+This is a managed device. Access to Cloud is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Prístup na Cloud server uzamknutý. Pre viac informácií kontaktujte: {contactInfo}.
+This is a managed device. Access to Goodreads on Kindle is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Prístup na Goodreads uzamknutý. Pre viac informácií kontaktujte: {contactInfo}.
+This is a managed device. Active Content is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Aktívny obsah uzamknutý. Pre viac informácií kontaktujte: {contactInfo}.
+This is a managed device. Aeroplane Mode is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Letový režim uzamknutý. Pre viac informácií kontaktujte: {contactInfo}.
+This is a managed device. It has been instructed to reset to factory defaults, which will remove all downloaded and transferred content. This process will begin shortly. Please wait until this process is complete. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Kindle dostal pokyn na vyresetovanie do výrobných nastavení, ktoré spôsobí odstránenie všetkého stiahnutého a skopírovaného obsahu. Tento proces sa čoskoro spustí. Počkajte prosím na jeho úplné dokončenie. Pre viac informácií kontaktujte: {contactInfo}.
+This is a managed device. Kindle Store is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Obchod Kindle uzamknutý. Pre viac informácií kontaktujte: {contactInfo}.
+This is a managed device. MP3 Player is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. MP3 prehrávač uzamknutý. Pre viac informácii kontaktujte: {contactInfo}.
+This is a managed device. Settings Menu is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Menu Nastavenia uzamknuté. Pre viac informácii kontaktujte: {contactInfo}.
+This is a managed device. Social Network access is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Prístup na sociálnu sieť uzamknutý. Pre viac informácii kontaktujte: {contactInfo}.
+This is a managed device. Toggle Wireless is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Prepínač bezdrôtovej siete uzamknutý. Pre viac informácii kontaktujte: {contactInfo}.
+This is a managed device. Web Browser is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Internetový prehliadač uzamknutý. Pre viac informácii kontaktujte: {contactInfo}.
+This is a managed device. Wi-Fi Settings configuration is locked. For information, contact: {contactInfo}.	Toto je spravované zariadenie. Nastavenie konfigurácie Wi-Fi uzamknuté. Pre viac informácii kontaktujte: {contactInfo}.
+To play MP3 files, you must first copy them from your computer to the "music" folder on your Kindle.	Ak chcete počúvať MP3 skladby, musíte ich najskôr skopírovať z vášho počítača do zložky "music" vo vašom Kindly.
+Toggle Wireless Locked	Prepínač bezdrôtovej siete uzamknutý
+Turn On Wireless?	Zapnúť bezdrôtovú sieť?
+Unable to Open Content	Nepodarilo sa otvoriť obsah
+Unable to enter shipping mode owing to battery level falling below permissible range.	Nepodarilo sa nastaviť prepravný mód z dôvodu nízkej úrovne batérie, ktorá presiahla povolenú úroveň.
+Unable to enter shipping mode owing to missing dictionaries or fonts.	Nepodarilo sa nastaviť prepravný mód z dôvodu chýbajúcich slovníkov alebo písiem.
+Update Later	Aktualizovať neskôr
+Update Now	Aktualizovať teraz
+Updating Fonts	Aktualizujem písma
+Very Low Battery	Úroveň batérie príliš nízka
+We are unable to process your request. Please try again later.	Nie sme schopní spracovať vašu požiadavku. Skúste to prosím neskôr.
+Web Browser Locked	Internetový prehliadač uzamknutý
+Wi-Fi Login Required	Vyžadované prihlásenie do Wi-Fi
+Wi-Fi Settings Locked	Nastavenie Wi-Fi uzamknuté
+Wireless is turned off. Do you want to turn on wireless?	Bezdrôtová sieť vypnutá. Chcete ju zapnnúť?
+You already have {count,plural,=0 {no orders pending} one {an order pending} other { # orders pending}} for this special offer.	Momentálne máte {count,plural,=0 {0 nevybavených objednávok} one {nevybavenú objednávku} other { # nevybavených objednávok}} pre túto špeciálnu ponuku.
+You must register your Kindle to make a purchase.	Ak chcete nakupovať, musíte si váš Kindle zaregistrovať.
+Your Kindle fonts are updating. This can take a moment...	Aktualizujú sa písma. Môže to chvíľu trvať...
+Your Kindle is unable to establish a wireless connection. Do you want to turn off Aeroplane Mode?	Váš Kindle nie je chopný nadviazať bezdrôtové pripojenie. Chcete vypnúť letový režim?
+Your battery is almost depleted and your Kindle will soon shut down. To continue using your Kindle, connect it to a power source.	Batéria takmer vyčerpaná, váš Kindle sa čoskoro vypne. Ak chcete pokračovať v jeho používaní, pripojte napájanie.
+Your battery is running low. Please charge your Kindle. If your wireless is turned on, try turning it off to extend your reading time.	Nízka úroveň batérie, nabite si prosím Kindle. Ak je zapnutá bezdrôtová sieť, môžte ju výpnúť, predĺžite tým čas čítania.
+Your battery is running low. Please charge your Kindle.	Nízka úroveň batérie, nabite si prosím Kindle.
+Your purchase could not be completed. This offer has expired.	Nepodarilo sa dokončiť nákup. Táto ponuka skončila.
+Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	Nepodarilo sa dokončiť nákup. Máme technické problémy. Skúste to prosím znovu.
+Your purchase could not be completed. You have reached the maximum purchase quantity.	Nepodarilo sa dokončiť nákup. Dosiahli ste maximálny počet nákupov.
